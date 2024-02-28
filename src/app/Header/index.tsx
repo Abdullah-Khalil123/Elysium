6df253473ed85029217c355606262521746745fe
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-02-28 21:48:39 +0500

    date add

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,10 +1,42 @@
 import style from "./Header.module.css";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const Header = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = String(date.getDate()).padStart(2, "0");
+
   return (
     <div className={style.Header}>
       <div></div>
-      <p>Friday, Novemever 16 2022</p>
+      <p>
+        {daysOfWeek[date.getDay()]}, {months[month] + " " + day + " " + year}
+      </p>
       <button>Create Booking</button>
     </div>
   );
