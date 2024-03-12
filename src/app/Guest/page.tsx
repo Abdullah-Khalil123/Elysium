9211f3a48889ef62024ca52eb7e4b5b2649eae9e
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-03-12 22:46:42 +0500

    fixes

--- src/app/Guest/page.tsx
+++ src/app/Guest/page.tsx
@@ -78,9 +78,6 @@ const Guest = () => {
         <div className={style.pageGuestSelectors}>
           <p>1</p>
           <p>2</p>
-          <p>3</p>
-          <p>4</p>
-          <p>5</p>
         </div>
         <button>Next</button>
       </div>
