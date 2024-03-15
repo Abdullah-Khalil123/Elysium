28628e893bc55003caa059b7adf5402e10d67745
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-03-16 00:52:31 +0500

    button create page

--- src/app/Create_Booking/page.tsx
+++ src/app/Create_Booking/page.tsx
@@ -1,5 +1,29 @@
+import DatePicker from "antd/es/date-picker";
+import Select from "antd/es/select";
+import style from "./booking.module.css";
+
 const createBooking = () => {
-  return <div>page</div>;
+  return (
+    <div className={style.booking}>
+      <h4>Add New Booking</h4>
+      <div className={style.SelectDate}>
+        <Select
+          className={style.select}
+          defaultValue={{ value: 0, label: "501" }}
+          options={[
+            { value: 0, label: "501" },
+            { value: 1, label: "601" },
+          ]}
+        />
+        <DatePicker className={style.DatePicker} />
+      </div>
+      <input className={style.inputAmount} placeholder="Amount" type="number" />
+      <div className={style.buttons}>
+        <button>Cancel</button>
+        <button>Create Booking</button>
+      </div>
+    </div>
+  );
 };
 
 export default createBooking;
