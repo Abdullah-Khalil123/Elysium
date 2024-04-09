9126a67c85e88319bb08bd8d431be514cd0278ab
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-04-10 01:48:52 +0500

    New Entry

--- src/app/Create_Booking/AddExpense/index.tsx
+++ src/app/Create_Booking/AddExpense/index.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import style from "./addExpense.module.css";
 import { Select } from "antd";
+import URI from "../../../Data/API";
 
 const addExpense = (props: {
   showExpense: Dispatch<SetStateAction<boolean>>;
@@ -9,6 +10,29 @@ const addExpense = (props: {
     { value: 0, label: "501" },
     { value: 1, label: "601" },
   ];
+
+  var roomid: number = roomOptions[0].value,
+    discrip: string,
+    amount: number,
+    date: string;
+
+  async function submitExpense() {
+    const uri = URI + "/api/addExpense";
+    const data = {
+      roomid: roomid,
+      discrip: discrip,
+      amount: amount,
+      date: date,
+    };
+    await fetch(uri, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+
   return (
     <div className={style.addExpense}>
       <p>Add Expense </p>
@@ -20,11 +44,35 @@ const addExpense = (props: {
         defaultValue={roomOptions[0]}
         className={style.Select}
       />
-      <input type="text" placeholder="Discription" />
-      <input type="text" placeholder="Amount" />
-      <input type="month" />
+      <input
+        type="text"
+        placeholder="Discription"
+        onChange={(e) => {
+          discrip = e.target.value;
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Amount"
+        onChange={(e) => {
+          amount = parseInt(e.target.value);
+        }}
+      />
+      <input
+        type="month"
+        onChange={(e) => {
+          date = e.target.value;
+        }}
+      />
       <div className={style.buttons}>
-        <button onClick={() => props.showExpense(false)}>Create Booking</button>
+        <button
+          onClick={() => {
+            submitExpense();
+            props.showExpense(false);
+          }}
+        >
+          Create Booking
+        </button>
       </div>
     </div>
   );
